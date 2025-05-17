26c17b4bc1d3251f94b728df67cffa60e9b0d49b
Ariel-cano <vaskaivanov856@gmail.com>
2025-05-17 15:05:58 +0400

    feat: add basic navigation with routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,12 @@
 import { Routes } from '@angular/router';
+import {UsersListComponent} from './pages/users-list/users-list.component';
+import {UserDetailsComponent} from './pages/user-details/user-details.component';
+import {UserFormComponent} from './pages/user-form/user-form.component';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: '', pathMatch: 'full', redirectTo: '/users' },
+  {path: 'users', component: UsersListComponent},
+  {path: 'users/:id', component: UserDetailsComponent},
+  {path: 'users/:id/edit', component: UserFormComponent},
+  {path: 'users/new', component: UserFormComponent},
 ];
