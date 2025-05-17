e326a6d9b773d6082b827b3a005d368b667ed657
Ariel-cano <vaskaivanov856@gmail.com>
2025-05-17 14:56:24 +0400

    feat: add ng-zorro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-Yadro-task';
+  title(title: any) {
+      throw new Error('Method not implemented.');
+  }
+  isCollapsed = false;
 }
