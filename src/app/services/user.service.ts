867bed5480e5d244593855604bce7b2048e76c0c
Ariel-cano <vaskaivanov856@gmail.com>
2025-05-17 16:40:19 +0400

    feat: add user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IUser } from '../model/user.model';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) {}
+
+  getAllUsers(): Observable<IUser[]> {
+    return this.http.get<IUser[]>(environment.API_URL);
+  }
+
+  getUserDetailsById(id: number): Observable<IUser> {
+    return this.http.get<IUser>(`${environment.API_URL}/${id}`);
+  }
+
+  createUser(user: IUser): Observable<IUser> {
+    return this.http.post<IUser>(environment.API_URL, user);
+  }
+
+  updateUser(user: IUser, id: number): Observable<IUser> {
+    return this.http.put<IUser>(`${environment.API_URL}/${id}`, user);
+  }
+
+  deleteUser(id: number): Observable<IUser> {
+    return this.http.delete<IUser>(`${environment.API_URL}/${id}`);
+  }
+}
